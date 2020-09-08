93c828ed0124dbf555ef69aa4b90185605180ad2
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-08 11:34:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,10 @@ var playlist = {}
 
 function updatePlaylist(playlist, artist, song) {
   playlist.artist = song;
-  return 
+  return playlist
 }
 
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist
+  return playlist
+}
